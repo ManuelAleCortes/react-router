30bd560c6c2cc98eeeef7beadd6670e3728b32d7
Manuel Cortes <manuel_ale.cortes@uao.edu.co>
2024-03-29 13:31:18 -0500

    Web actividad 2
    Avancé hasta la mitad del doc

--- src/index.js
+++ src/index.js
@@ -2,11 +2,35 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import ErrorPage from "./error-page";
+import Contact, {loader as contactLoader,} from "./routes/contact";
+import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
+import EditContact, {action as editAction,} from "./routes/edit";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+    ],
+  },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
   },
 ]);
 
